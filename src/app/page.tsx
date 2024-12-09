3d01754d94916a8f5bb2c5629966dbea3f206151
Abdul Rehman <ar03353505910@gmail.com>
2024-12-09 18:55:51 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
-import Image from "next/image";
+'use lient'
 
-export default function Home() {
-  return (
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import TopCategories from "./components/TopCategories";
+import LatestProducts from "./components/LatestProducts";
+import Feature from "./components/FeatureProduct";
+import Offers from "./components/Offers";
+import Unique from "./components/Unique";
+import TrendingProducts from "./components/TrendingProducts";
+import Discount from "./components/Discount";
+import BlogSection from "./components/BlogSection";
+import Newslater from "./components/Newslater";
 
-  );
+function Homepage(){
+  return(
+    <div>
+      <Header />
+      <Hero />
+      <Feature />
+      <LatestProducts />
+      <Offers />
+      <Unique />
+      <TrendingProducts />
+      <Discount />
+      <TopCategories />
+      <Newslater />
+      <BlogSection />
+      <Footer />
+    </div>
+  )
 }
+
+export default Homepage;
